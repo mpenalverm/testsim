af8f961645398115b3ba89688830811a1fdef0cd
Marcos Peñalver <markttes@gmail.com>
2019-10-25 20:03:52 +0200

    Added complete run_pipeline.sh

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,24 +1,25 @@
 #get the genome
-#mkdir -p res/genome
-#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
-#mv GCF_000005845.2_ASM584v2_genomic.fna.gz res/genome/ecoli.fasta.gz
-#gunzip -k res/genome/ecoli.fasta.gz
+echo "Starting pipeline"
+mkdir -p res/genome
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
+mv GCF_000005845.2_ASM584v2_genomic.fna.gz res/genome/ecoli.fasta.gz
+gunzip -k res/genome/ecoli.fasta.gz
 #Genome index!
-#echo
-#echo "Running STAR index..."
-#mkdir -p res/genome/star_index
-#STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/genome/star_index/ --genomeFastaFiles res/genome/ecoli.fasta --genomeSAindexNbases 9
-#echo "...STAR index complete!"
-#echo
-#echo "Starting Sample analysis"
-#Running the analyses for each sample ID
-#for sid in $(ls data/*.fastq.gz | cut -d "_" -f1 | sed 's:data/::' | sort | uniq)
-#do
-#	bash scripts/analyse_sample.sh $sid
-#done
-#echo "Sample analysis complete"
-#echo
+echo
+echo "Running STAR index..."
+mkdir -p res/genome/star_index
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/genome/star_index/ --genomeFastaFiles res/genome/ecoli.fasta --genomeSAindexNbases 9
+echo "...STAR index complete!"
+echo
+echo "Starting Sample analysis"
+#Running the analysis for each sample ID
+for sid in $(ls data/*.fastq.gz | cut -d "_" -f1 | sed 's:data/::' | sort | uniq)
+do
+	bash scripts/analyse_sample.sh $sid
+done
+echo "Sample analysis complete"
+echo
 #After all samples analysed, we do the Multiqc"
 echo "Preparing MultiQC report"
-multiqc -o out/multiqc $WD
+multiqc -o out/multiqc .
 echo "MultiQC report generated and ready"
