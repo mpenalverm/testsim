7528e63dbfe73c43911d87061dd216bb9addab6a
Marcos Peñalver <markttes@gmail.com>
2019-10-25 19:19:51 +0200

    Getting Sample ID list

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,11 +1,19 @@
 #get the genome
-mkdir -p res/genome
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
-mv GCF_000005845.2_ASM584v2_genomic.fna.gz res/genome/ecoli.fasta.gz
-gunzip -k res/genome/ecoli.fasta.gz
+#mkdir -p res/genome
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
+#mv GCF_000005845.2_ASM584v2_genomic.fna.gz res/genome/ecoli.fasta.gz
+#gunzip -k res/genome/ecoli.fasta.gz
 #Genome index!
-echo
-echo "Running STAR index..."
-mkdir -p res/genome/star_index
-STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/genome/star_index/ --genomeFastaFiles res/genome/ecoli.fasta --genomeSAindexNbases 9
-echo "...STAR index complete!"
+#echo
+#echo "Running STAR index..."
+#mkdir -p res/genome/star_index
+#STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/genome/star_index/ --genomeFastaFiles res/genome/ecoli.fasta --genomeSAindexNbases 9
+#echo "...STAR index complete!"
+#echo
+echo "Starting Sample analysis"
+#Running the analyses for each sample ID
+for sid in $(ls data/*.fastq.gz | cut -d "_" -f1 | sed 's:data/::' | sort | uniq)
+do
+  echo $sid
+done
+
